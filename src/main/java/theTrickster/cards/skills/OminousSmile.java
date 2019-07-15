1bc26c3fbe23d9f56eb73eacfdafb73d56bf1391
Hyvel <hyvel@protonmail.com>
2019-07-15 20:22:03 +0200

    Added new card art for Frenzy. Used old art for ominous smile.
    Also fixed a typo where ominous smile was common instead of uncommon.

--- src/main/java/theTrickster/cards/skills/OminousSmile.java
+++ src/main/java/theTrickster/cards/skills/OminousSmile.java
@@ -21,16 +21,14 @@ public class OminousSmile extends AbstractDynamicCard {
     // TEXT DECLARATION
 
     public static final String ID = TricksterMod.makeID(OminousSmile.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");// "public static final String IMG = makeCardPath("OminousSmile.png");
-    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
-
+    public static final String IMG = makeCardPath("skills/OminousSmile.png");
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheTrickster.Enums.COLOR_BROWN;
